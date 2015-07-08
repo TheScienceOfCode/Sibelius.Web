bd4af01ac679021d08d89fe428dc1500b337aaac
Daniel Cañizares Corrales <dacanizares@outlook.com>
2015-07-08 12:18:20 -0500

    Hidding connection details!
    Don't worry, we changed the MongoLab config.

--- Sibelius.Web/Data/Global.cs
+++ Sibelius.Web/Data/Global.cs
@@ -8,8 +8,7 @@ namespace Sibelius.Web.Data
     public static class Global
     {
         public static readonly string Connection = 
-            // System.Environment.GetEnvironmentVariable("CUSTOMCONNSTR_MONGOLAB_URI"); 
-            "mongodb://webapp:Tsocdb123*@ds062097.mongolab.com:62097/TsocDB";
+            System.Environment.GetEnvironmentVariable("CUSTOMCONNSTR_MONGOLAB_URI");            
             // USE A STRING LIKE THIS WHEN DEV: "mongodb://localhost/tsoc";
     }
 }
