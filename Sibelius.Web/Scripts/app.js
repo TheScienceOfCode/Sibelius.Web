855540849a240888517fd1d623702ca0b4bb9e7f
Daniel Cañizares Corrales <dacanizares@outlook.com>
2015-07-08 12:18:37 -0500

    Courses UI improvements!

--- Sibelius.Web/Scripts/app.js
+++ Sibelius.Web/Scripts/app.js
@@ -1,21 +1,35 @@
 ﻿/// Toogleable elements with fixed menu
 var fixed_menu = -1;
+var fixed_status = false;
 $(function () {
     if ($('#fixed-menu').offset() == undefined) return;
     fixed_menu = $('#fixed-menu').offset().top - 50;
     $(window).scroll(function () {
         if ($(window).scrollTop() > fixed_menu) {
-            $('#fixed-menu').addClass('fixed');
-            $('#fixed-menu').addClass('well');
-            $('#fixed-menu').addClass('well-sm');
-            $('a[data-toggle]').removeClass('btn-primary');
-            $('a[data-toggle]').addClass('btn-default');
+            if (fixed_status) return;
+            fixed_status = true;
+            $('#fixed-menu').fadeOut('fast', function () {
+                $('#fixed-menu-empty').fadeIn('fast');
+                $('#fixed-menu').addClass('fixed');
+                $('#fixed-menu').addClass('well');
+                $('#fixed-menu').addClass('well-sm');
+                $('a[data-toggle]').removeClass('btn-primary');
+                $('a[data-toggle]').addClass('btn-default');
+                $('#fixed-menu').fadeIn('fast');                
+            });            
         } else {
-            $('#fixed-menu').removeClass('fixed');
-            $('#fixed-menu').removeClass('well');
-            $('#fixed-menu').removeClass('well-sm');
-            $('a[data-toggle]').removeClass('btn-default');
-            $('a[data-toggle]').addClass('btn-primary');
+            if (!fixed_status) return;
+            fixed_status = false;
+            $('#fixed-menu').fadeOut('fast', function () {                
+                $('#fixed-menu').removeClass('fixed');
+                $('#fixed-menu').removeClass('well');
+                $('#fixed-menu').removeClass('well-sm');
+                $('a[data-toggle]').removeClass('btn-default');
+                $('a[data-toggle]').addClass('btn-primary');
+                $('#fixed-menu-empty').fadeOut('fast', function () {
+                    $('#fixed-menu').fadeIn('fast');
+                });                
+            });
         }
     });
 });
@@ -36,9 +50,9 @@ $(function () {
 
             var element = current.attr('data-toggle');
             current.addClass('active');
-            $(element).fadeIn('slow', function () {
+            $(element).fadeIn('fast', function () {
                 $('html, body').animate({
-                    scrollTop: fixed_menu - 70
+                    scrollTop: fixed_menu - 20
                 });
             });
         });       
