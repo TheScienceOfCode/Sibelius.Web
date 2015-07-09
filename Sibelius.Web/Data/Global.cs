a8db783c66dc314579d7da9bdf95be0249602515
Daniel Cañizares Corrales <dacanizares@outlook.com>
2015-07-08 19:21:00 -0500

    UI Fixes
    Font sizes and spacing

--- Sibelius.Web/Data/Global.cs
+++ Sibelius.Web/Data/Global.cs
@@ -7,7 +7,7 @@ namespace Sibelius.Web.Data
 {
     public static class Global
     {
-        public static readonly string Connection = 
+        public static readonly string Connection =
             System.Environment.GetEnvironmentVariable("CUSTOMCONNSTR_MONGOLAB_URI");            
             // USE A STRING LIKE THIS WHEN DEV: "mongodb://localhost/tsoc";
     }
