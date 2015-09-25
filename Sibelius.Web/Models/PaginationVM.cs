b4d57e623dbe1f132de347977e27f8813a6561dd
Daniel Cañizares Corrales <dacanizares@outlook.com>
2015-09-25 11:07:10 -0500

    Pagination VM

---
+++ Sibelius.Web/Models/PaginationVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sibelius.Web.Models
+{
+    public class PaginationVM
+    {
+        public int Pages { get; set; }
+        public int CurPage { get; set; }
+        public string Url { get; set; }
+    }
+}
