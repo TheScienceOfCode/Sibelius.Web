732417852fa45eb4b7315eb713f82e6bd867df22
Daniel Cañizares Corrales <dacanizares@outlook.com>
2016-08-25 00:11:45 -0500

    Revert "Missing ; -.-"
    This reverts commit f46aae47aeb2033a7adcfb88253e509c52762d09.

--- Sibelius.Web/Data/Global.cs
+++ Sibelius.Web/Data/Global.cs
@@ -8,8 +8,8 @@ namespace Sibelius.Web.Data
     public static class Global
     {
         public static readonly string Connection =
-             //Environment.GetEnvironmentVariable("CUSTOMCONNSTR_MONGOLAB_URI");
-             "mongodb://localhost/tsoc";                
+             Environment.GetEnvironmentVariable("CUSTOMCONNSTR_MONGOLAB_URI");
+             //"mongodb://localhost/tsoc";                
 
         public static readonly string DefaultMainImg =
             Environment.GetEnvironmentVariable("APPSETTING_DEFAULT_MAINIMG_URL");
