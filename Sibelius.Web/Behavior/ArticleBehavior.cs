068c5f7a1bd602a62792c1a767e43af1c98c664c
Daniel Cañizares Corrales <dacanizares@outlook.com>
2016-09-08 01:36:37 -0500

    Articles fixes
    Removing frontpage int (wip), now ordering by date.

--- Sibelius.Web/Behavior/ArticleBehavior.cs
+++ Sibelius.Web/Behavior/ArticleBehavior.cs
@@ -32,8 +32,7 @@ namespace Sibelius.Web.Behavior
         {
             var articles = unit.Articles
                 .Where(c => c.Visible == true)
-                .OrderByDescending(c => c.FrontPage)
-                .ThenByDescending(c => c.Date).ToList();
+                .OrderByDescending(c => c.Date).ToList();
 
             licenseableBehavior.AddLicense(articles);
             return articles;
