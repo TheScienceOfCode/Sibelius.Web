43601a8cf9c204bc64600b4aef19eb3cd61ca291
dacanizares <dacanizares@outlook.com>
2020-09-11 01:53:05 -0500

    Reusing MongoRepos
     (kind of dirty, but without it, this new version runs really slow)

--- Sibelius.Web/Data/UnitOfWork.cs
+++ Sibelius.Web/Data/UnitOfWork.cs
@@ -9,25 +9,24 @@ namespace Sibelius.Web.Data
 {
     public class UnitOfWork
     {
-        public MongoRepository<Course> Courses;
-        public MongoRepository<Article> Articles;
-        public MongoRepository<Post> Posts;
-        public MongoRepository<PostSection> PostSections;
-        public MongoRepository<Collaborator> Collaborators;
-        public MongoRepository<Question> Questions;
-        public MongoRepository<License> Licenses;
-        public MongoRepository<Asset> Assets;
+        public static MongoRepository<Course> _courses = new MongoRepository<Course>(Global.Connection);
+        public static MongoRepository<Article> _articles = new MongoRepository<Article>(Global.Connection);
+        public static MongoRepository<Post> _posts = new MongoRepository<Post>(Global.Connection);
+        public static MongoRepository<PostSection> _postSections = new MongoRepository<PostSection>(Global.Connection);
+        public static MongoRepository<Collaborator> _collaborators = new MongoRepository<Collaborator>(Global.Connection);
+        public static MongoRepository<Question> _questions = new MongoRepository<Question>(Global.Connection);
+        public static MongoRepository<License> _licenses = new MongoRepository<License>(Global.Connection);
+        public static MongoRepository<Asset> _assets = new MongoRepository<Asset>(Global.Connection);
 
-        public UnitOfWork()
-        {
-            Courses = new MongoRepository<Course>(Global.Connection);
-            Articles = new MongoRepository<Article>(Global.Connection);
-            Posts = new MongoRepository<Post>(Global.Connection);
-            PostSections = new MongoRepository<PostSection>(Global.Connection);
-            Collaborators = new MongoRepository<Collaborator>(Global.Connection);
-            Questions = new MongoRepository<Question>(Global.Connection);
-            Licenses = new MongoRepository<License>(Global.Connection);
-            Assets = new MongoRepository<Asset>(Global.Connection);
-        }
+        public MongoRepository<Course> Courses { get { return _courses; } }
+        public MongoRepository<Article> Articles { get { return _articles; } }
+        public MongoRepository<Post> Posts { get { return _posts; } }
+        public MongoRepository<PostSection> PostSections { get { return _postSections; } }
+        public MongoRepository<Collaborator> Collaborators { get { return _collaborators; } }
+        public MongoRepository<Question> Questions { get { return _questions; } }
+        public MongoRepository<License> Licenses { get { return _licenses; } }
+        public MongoRepository<Asset> Assets { get { return _assets; } }
+
+        public UnitOfWork() { }
     }
 }
